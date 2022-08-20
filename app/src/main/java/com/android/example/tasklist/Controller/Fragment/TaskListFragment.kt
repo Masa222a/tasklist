bc15b81f0c7ac6d01cc4f4dca3514a46b2d21490
Masa222a <guitarstring0716@gmail.com>
2022-08-20 10:17:43 +0900

    スワイプ時にタスクを消去

--- app/src/main/java/com/android/example/tasklist/Controller/Fragment/TaskListFragment.kt
+++ app/src/main/java/com/android/example/tasklist/Controller/Fragment/TaskListFragment.kt
@@ -1,5 +1,8 @@
 package com.android.example.tasklist.Controller.Fragment
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,7 +10,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.example.tasklist.Model.Task
 import com.android.example.tasklist.R
 import com.android.example.tasklist.SharedPreferencesManager
@@ -60,6 +65,9 @@ class TaskListFragment : Fragment(), AddTaskFragment.AddTaskListener{
             }
         })
 
+        val swipeToDismissTouchHelper = getSwipeToDismissTouchHelper(adapter)
+        swipeToDismissTouchHelper.attachToRecyclerView(recyclerView)
+
         return view
     }
 
@@ -85,4 +93,62 @@ class TaskListFragment : Fragment(), AddTaskFragment.AddTaskListener{
         adapter.updateTaskList(taskList)
     }
 
+
+    private fun getSwipeToDismissTouchHelper(adapter: RecyclerView.Adapter<TaskListAdapter.ViewHolder>) =
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                taskList.removeAt(viewHolder.adapterPosition)
+                adapter.notifyItemRemoved(viewHolder.adapterPosition)
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+                val itemView = viewHolder.itemView
+                val background = ColorDrawable()
+                background.color = Color.parseColor("#f44336")
+                if (dX < 0)
+                    background.setBounds(
+                        itemView.right + dX.toInt(),
+                        itemView.top,
+                        itemView.right,
+                        itemView.bottom
+                    )
+                else
+                    background.setBounds(
+                        itemView.left,
+                        itemView.top,
+                        itemView.left + dX.toInt(),
+                        itemView.bottom
+                    )
+
+                background.draw(c)
+            }
+        })
 }
